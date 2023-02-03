7401246c0b2deb14055f3e2586e1d1931fd12195
Rodgi <124323104+Rodgi@users.noreply.github.com>
2023-02-03 04:24:27 +0000

    Learning

---
+++ UWWCS220/UWW CS220/Week2/Labs/ZipCodeValidation.java
@@ -0,0 +1,49 @@
+package Week2.Labs;
+
+import javax.swing.JOptionPane;
+
+public class ZipCodeValidation {
+
+    public static void main(String[] args) {
+        // Use JOptionPane class to create an input dialog box for user input zip code
+        String userInput = JOptionPane.showInputDialog("Enter a zip code; we will validate it for you.");
+        boolean validInput = false;
+        // Use a loop to have the user re-enter the zip code until the entered code is
+        // valid, or enter q/Q to quit
+        if(userInput.equalsIgnoreCase("Q"))
+            System.out.println("Sucessfully Quit Program.");
+        while (validInput | !userInput.equalsIgnoreCase("Q")) {
+            
+            if (userInput.length() == 5 ) {
+                char[] charArray = new char[userInput.length()];
+                int numCharValid = 0;
+                for (int i = 0; i < userInput.length(); i++) {
+                    charArray[i] = userInput.charAt(i);
+                }
+
+                for (char ca : charArray) {
+                    if (Character.isDigit(ca))
+                        numCharValid++;
+                }
+                if (numCharValid == 5) {
+                    validInput = true;
+                    break;
+                } else {
+                    System.out.printf("%s is an invalid zip code! Invalid Character!%n", userInput);
+                    userInput = JOptionPane.showInputDialog("Enter a zip code; we will validate it for you.");
+                }
+
+            } else {
+                System.out.printf("%s is an invalid zip code! Too Long or Too Short!%n", userInput);
+                userInput = JOptionPane.showInputDialog("Enter a zip code; we will validate it for you.");
+            }
+            
+        }
+        // Print out validation result for each user inputted zip code in loop
+        if (validInput)
+            System.out.printf("%s is a valid zip code!", userInput);
+        
+
+
+    }
+}
---
+++ UWWCS220/UWW CS220/Week2/TakeHomeLab/DollarConvert.java
@@ -0,0 +1,101 @@
+package Week2.TakeHomeLab;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+
+
+public class DollarConvert {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double[] userInputs = new double[5];
+
+        for (int i = 0; i < userInputs.length; i++) {
+            try {
+                System.out.println("Enter dollar amount.");
+                userInputs[i] = scnr.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid dollar amount.");
+                scnr.next();
+                i--;
+            }
+            catch (NoSuchElementException | IllegalStateException e) {
+                System.out.println("Invalid input. Please enter a valid dollar amount.");
+                scnr.next();
+                i--;
+            }
+        }
+        for (double dollarsValue : userInputs) {
+            int dollars = (int) dollarsValue;
+            int dollarActual = (int) dollarsValue;
+            int hundreds = dollars / 100; dollars = dollars % 100;
+            int fifties = dollars / 50; dollars = dollars % 50;
+            int twenties = dollars / 20; dollars = dollars % 20;
+            int tens = dollars / 10; dollars = dollars % 10;
+            int fives = dollars / 5; dollars = dollars % 5;
+            int ones = dollars;
+            int cents = (int) Math.round((dollarsValue - (int) dollarsValue) * 100);
+            int quarters = cents / 25;  cents = cents % 25;
+            int dimes = cents / 10; cents = cents % 10;
+            int nickels = cents / 5; cents = cents % 5;
+            int pennies = cents;
+            if (cents > 0)
+                printResults(dollarsValue, dollarActual, hundreds, fifties, twenties, tens, fives, ones, quarters,
+                        dimes, nickels, pennies);
+            else
+                printResult(dollarsValue, dollarActual, hundreds, fifties, twenties, tens, fives, ones);
+
+        }
+        scnr.close();
+    }
+
+    public static void printResult(double dollarsValue, int dollarActual, int hundreds, int fifties, int twenties,
+            int tens, int fives, int ones) {
+        if (dollarActual < 5)
+            System.out.printf("%n$%.2f sdf can be reduced to %nOnes: %d%n%n", dollarsValue, ones);
+        else if (dollarActual < 10)
+            System.out.printf("%n$%.2f can be reduced to %nFives: %d%nOnes: %d%n%n", dollarsValue, fives, ones);
+        else if (dollarActual < 20)
+            System.out.printf("%n$%.2f can be reduced to %nTens: %d%nFives: %d%nOnes: %d%n%n", dollarsValue, tens,
+                    fives, ones);
+        else if (dollarActual < 50)
+            System.out.printf("%n$%.2f can be reduced to %nTwenties: %d%nTens: %d%nFives: %d%nOnes: %d%n%n",
+                    dollarsValue, twenties, tens, fives, ones);
+        else if (dollarActual < 100)
+            System.out.printf(
+                    "%n$%.2f can be reduced to %nFifties: %d%nTwenties: %d%nTens: %d%nFives: %d%nOnes: %d%n%n",
+                    dollarsValue, fifties, twenties, tens, fives, ones);
+        else
+            System.out.printf(
+                    "%n$%.2f can be reduced to %nHundreds: %d%nFifties: %d%nTwenties: %d%nTens: %d%nFives: %d%nOnes: %d%n%n",
+                    dollarsValue, hundreds, fifties, twenties, tens, fives, ones);
+
+    }
+
+    public static void printResults(double dollarsValue, int dollarActual, int hundreds, int fifties, int twenties,
+            int tens, int fives, int ones, int quarters, int dimes, int nickels, int pennies) {
+        if (dollarActual < 5)
+            System.out.printf(
+                    "%n$%.2f sdf can be reduced to %Ones: %d%nQuarters: %d%nDimes: %d%nNickels: %d%nPennies: %d%n%n",
+                    dollarsValue, ones, quarters, dimes, nickels, pennies);
+        else if (dollarActual < 10)
+            System.out.printf(
+                    "%n$%.2f can be reduced to %nFives: %d%nOnes: %d%nQuarters: %d%nDimes: %d%nNickels: %d%nPennies: %d%n%n",
+                    dollarsValue, fives, ones, quarters, dimes, nickels, pennies);
+        else if (dollarActual < 20)
+            System.out.printf(
+                    "%n$%.2f can be reduced to %nTens: %d%nFives: %d%nOnes: %d%nQuarters: %d%nDimes: %d%nNickels: %d%nPennies: %d%n%n",
+                    dollarsValue, tens, fives, ones, quarters, dimes, nickels, pennies);
+        else if (dollarActual < 50)
+            System.out.printf(
+                    "%n$%.2f can be reduced to %nTwenties: %d%nTens: %d%nFives: %d%nOnes: %d%nQuarters: %d%nDimes: %d%nNickels: %d%nPennies: %d%n%n",
+                    dollarsValue, twenties, tens, fives, ones, quarters, dimes, nickels, pennies);
+        else if (dollarActual < 100)
+            System.out.printf(
+                    "%n$%.2f can be reduced to %nFifties: %d%nTwenties: %d%nTens: %d%nFives: %d%nOnes: %d%nQuarters: %d%nDimes: %d%nNickels: %d%nPennies: %d%n%n",
+                    dollarsValue, fifties, twenties, tens, fives, ones, quarters, dimes, nickels, pennies);
+        else
+            System.out.printf(
+                    "%n$%.2f can be reduced to %nHundreds: %d%nFifties: %d%nTwenties: %d%nTens: %d%nFives: %d%nOnes: %d%nQuarters: %d%nDimes: %d%nNickels: %d%nPennies: %d%n%n",
+                    dollarsValue, hundreds, fifties, twenties, tens, fives, ones, quarters, dimes, nickels, pennies);
+    }
+}
