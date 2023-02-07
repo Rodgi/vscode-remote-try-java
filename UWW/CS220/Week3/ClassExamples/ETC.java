3d902bd6d8846ff4b704396bfb4b9322a3669158
Rodgi <124323104+Rodgi@users.noreply.github.com>
2023-02-07 17:28:44 +0000

    Notes!

---
+++ UWW/CS220/Week3/ClassExamples/ETC.java
@@ -0,0 +1,15 @@
+package Week3.ClassExamples;
+
+public class ETC {
+    public static void main(String[] args) {
+        // Compound interest calculation using for loop
+        double amount;
+        double principal = 1000.0;
+        double rate = 0.035;
+        System.out.printf("%s%20s%n", "Year", "Amount on deposit");
+        for (int year = 1; year <= 10; year++) {
+            amount = principal * Math.pow(1.0 + rate, year);
+            System.out.printf("%4d%,20.2f%n", year, amount);
+        }
+    }
+}
