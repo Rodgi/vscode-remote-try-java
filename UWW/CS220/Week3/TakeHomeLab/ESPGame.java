9580b58a199b382ed5b5783df25c743db0405625
Rodgi <124323104+Rodgi@users.noreply.github.com>
2023-02-14 04:52:03 +0000

    Working on ESPGame

--- UWW/CS220/Week3/TakeHomeLab/ESPGame.java
+++ UWW/CS220/Week3/TakeHomeLab/ESPGame.java
@@ -2,17 +2,27 @@ package Week3.TakeHomeLab;
 
 import javax.swing.JOptionPane;
 import java.util.Random;
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class ESPGame {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         String userInput = "";
         String[] colors = {"Red", "Green", "Blue", "Yellow", "Purple"};
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+        int yellow = 0;
         //Loop for the user to enter a color until 'quit' is entered
         while(!userInput.equalsIgnoreCase("quit")){
             System.out.println("Colors Availiable: Red, Green, Blue, Yellow, Purple  ");
-            userInput = JOptionPane.showInputDialog("Enter a color: ");
+            //userInput = JOptionPane.showInputDialog("Enter a color: ");
+            userInput = input.nextLine();
             
+
+
         }
+
         
 
     }
